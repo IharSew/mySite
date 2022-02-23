c5d17dc268f5a39dee9b1e3a129a9970043cdb85
Ihar Syantovich <capo.by@gmail.com>
2022-02-23 18:00:06 +0300

    we want paginators

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls', namespace='blog')), # вреЕменная заглушка для перенаправления в blog из корня сайта
     path('blog/', include('blog.urls', namespace='blog')),
 ]
