b7c7a0e271e4f7c8be72b9297fb04cbc071ad7fa
capo <capo.by@gmail.com>
2022-03-16 17:52:17 +0300

    Paginator was added

--- blog/views.py
+++ blog/views.py
@@ -5,18 +5,24 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     object_list = Post.published.all()
-    # По 3 статьи на каждой странице.
+    # инициализируем объект класса Paginator, указав количество объектов
+    # на одной странице:
     paginator = Paginator(object_list, 3)
+    # извлекаем из запроса GET параметр Page:
     page = request.GET.get('page')
     try:
+        # получаем список объектов на нужной странице с помощью метода page()
+        # класса Paginator;
         posts = paginator.page(page)
     except PageNotAnInteger:
-            # Если страница не является целым числом, возвращаем первую страницу.
-            posts = paginator.page(1)
+        # если указанный параметр page не является целым числом, обращаемся
+        # к первой странице. Если page больше, чем общее количество страниц, то
+        # возвращаем последнюю:
+        posts = paginator.page(1)
     except EmptyPage:
         # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
         posts = paginator.page(paginator.num_pages)
-    return render(request,'blog/post/list.html', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
