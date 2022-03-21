9e855b86ba270fd5c06d24eaabc570936f588ee1
capo <capo.by@gmail.com>
2022-03-21 16:08:23 +0300

    Send Mail was added

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+# Мы создали класс формы, унаследованный от Form
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False, widget=forms.Textarea)
+
